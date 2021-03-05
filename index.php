8761d52a0a471aed4b8a298c9c57edae4b64c56c
Diego Quezada <dquezada-munoz@csumb.edu>
2021-03-04 19:50:45 -0800

    changes

--- index.php
+++ index.php
@@ -1,1 +1,47 @@
-Welcome!
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>A webpage about Diego Quezada-Munoz</title>
+        <link href="css/stylesPage.css"type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        
+        <h1>A webpage about Diego Quezada-Munoz</h1>
+        
+        <nav>
+            <a href="homePage.html">Home</a>
+            <a href="research.html">Research</a>
+            <a href="info.html">Info</a>
+            <a href="stories.html">Stories</a>
+        </nav>
+        
+        <div id="text">
+            <p> 
+                Welcome to my webpage! Peep my <a href="https://github.com/quez5481">GitHub Repository</a> 
+                and my <a href="https://www.linkedin.com/in/diegoQuezada-Munoz">LinkedIn Profile</a>. 
+            </p>
+        </div>
+        
+        <br/><br/>
+       
+        
+        <img src="images/jellyfish_5510_3675.jpg" width="1102" alt="jellyfish">
+        
+        <br/><br/>
+        
+        
+        
+        
+        <footer>
+            <br/><br/>
+            <div>
+                <hr>
+                Quezada-Munoz 2018&copy;<br/>
+                <strong>Disclaimer:</strong> This webpage is used for academic purposes only.<br/>
+            </div>
+            
+            <br/>
+            <img src="images/csumb_logo_150_86.jpg" alt="csumbLogo">
+        </footer>
+    </body>
+</html>
